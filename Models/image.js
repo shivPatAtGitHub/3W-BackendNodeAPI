781d9278f83ba06020905fc3ceba5821d0dbed02
Shiv Patil <smpatil@kenmarkitan.com>
2025-01-19 05:37:24 +0530

    added Image Model

---
+++ Models/image.js
@@ -0,0 +1,15 @@
+import Sequelize from "sequelize";
+import dbConnect from "../Utils/dbconnection.js";
+
+const Image_Model = dbConnect.define("image", {
+  imagePath: Sequelize.STRING,
+  imageURL: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      const path = this.getDataValue("imagePath");
+      return path ? `https://api.3w.in/${path}` : null;
+    },
+  },
+});
+
+export default Image_Model;
